d95022382ad7063e75f733fcd167ef2998e988fa
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-21 19:41:54 +0200

    Added permissions

---
+++ src/FileSystem/Permissions.java
@@ -0,0 +1,12 @@
+package FileSystem;
+
+/**
+ * Created by Ionita Cosmin on 12/21/2015.
+ */
+public class Permissions {
+
+    public static boolean read;
+    public static boolean write;
+
+    public static String userName;
+}
