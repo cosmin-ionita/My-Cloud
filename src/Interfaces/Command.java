fa429103e8a1a840f7fa18fd39508b8b5278e95b
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-23 16:49:48 +0200

    Added new methods

--- src/Interfaces/Command.java
+++ src/Interfaces/Command.java
@@ -1,8 +1,14 @@
 package Interfaces;
 
+import FileSystem.Directory;
+import FileSystem.File;
+
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
 public interface Command {
-     void execute(Repository repository);
+    void execute();
+    void execute(File file);
+    void execute(Directory directory);
+    void execute(Repository repository);
 }
