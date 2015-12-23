1cbc492740f19ec6cb8a0f4250ea1fb4751aa1b5
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-23 16:54:26 +0200

    Modified existing File System according to Abstract File System

--- src/FileSystem/Directory.java
+++ src/FileSystem/Directory.java
@@ -3,23 +3,66 @@ package FileSystem;
 import Interfaces.Command;
 import Interfaces.Repository;
 
-import java.util.Date;
-import java.util.List;
-
+import java.util.ArrayList;
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
-public class Directory implements Repository{
+public class Directory extends AbstractFileSystem {
 
-    private List<Repository> data;
+    private ArrayList<Repository> data;
 
-    private Permissions permissions;
+    private Directory parent;
 
-    private Date creationTime;
-
-    private int dimension;
+    public Directory(String name) {
+        this.name = name;
+        this.data = new ArrayList<>();
+    }
 
     public void accept(Command command) {
         command.execute(this);
+    }
+
+    public void addDirectory(String directoryName) {
+        data.add(new Directory(directoryName)); // legare de parent
+    }
+
+    public void addFile(String fileName) {
+        data.add(new File(fileName));
+    }
+
+    public Repository getNode(String nodeName) {
+
+        for (Repository repository : data) {
+
+            System.out.println(repository.getClass().toString().split(" ")[1]);
+
+            if (repository.getClass().toString().split(" ")[1].equals("FileSystem.Directory")) {
+                Directory directory = (Directory) repository;
+
+                if (directory.name.equals(nodeName))
+                    return directory;
+
+            } else if (repository.getClass().toString().split(" ")[1].equals("FileSystem.File")) {
+                File file = (File) repository;
+
+                if (file.name.equals(nodeName))
+                    return file;
+            }
+        }
+
+        return null;
+    }
+
+    public String printContent() {
+        String content = "";
+
+        for (int i = 0; i < data.size(); i++) {
+            content += data.get(i) + " ";
+        }
+        return content;
+    }
+
+    public String toString() {
+       return this.name;
     }
 }
--- src/FileSystem/File.java
+++ src/FileSystem/File.java
@@ -8,21 +8,21 @@ import java.util.Date;
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
-public class File implements Repository {
-
-    private String fileName;
-
-    private int dimension;
-
-    private Date creationTime;
-
-    private Permissions permissions;
+public class File extends AbstractFileSystem {
 
     private boolean isBinary;
 
     private byte[] content;
 
+    public File(String fileName) {
+        this.name = fileName;
+    }
+
     public void accept(Command command) {
         command.execute(this);
+    }
+
+    public String toString() {
+        return this.name;
     }
 }
