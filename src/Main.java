3bcf9b618f70eccd7f2c600e1b0edb4bcbe390bb
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-23 16:57:19 +0200

    Added testing commands

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import FileSystem.FileSystem;
+import Interfaces.Command;
+import Interfaces.Repository;
+import Utils.Parameters;
+
 public class Main {
 
     public static void main(String[] args) {
+        String command = "mkdir myFolder";
+
+        // parsing steps
+
+        Command myCommand = CommandFactory.getCommand("mkdir");
+
+        FileSystem fileSystem = FileSystem.getFileSystem();
+
+        Parameters.setParameters("myFolder");
+
+        myCommand.execute();
+
+
+
+
+        String command3 = "mkdir myFolder2";
+
+        // parsing steps
+
+        Command myCommand3 = CommandFactory.getCommand("mkdir");
+
+        FileSystem fileSystem2 = FileSystem.getFileSystem();
+
+        Parameters.setParameters("myFolder2");
+
+        myCommand.execute(fileSystem2.currentDirectory);
+
+
+        String command2 = "ls";
+
+        // parsing steps
+
+        Parameters.setParameters("");
+        Command myCommand2 = CommandFactory.getCommand("ls");
+
+        myCommand2.execute();
+
 
     }
 }
