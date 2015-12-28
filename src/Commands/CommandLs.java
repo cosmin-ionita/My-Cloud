b7372ef978ff7c8ded0a5e964659c1e206ca3995
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-28 20:57:18 +0200

    Added Ls command options logic

--- src/Commands/CommandLs.java
+++ src/Commands/CommandLs.java
@@ -1,27 +1,35 @@
 package Commands;
 
-import FileSystem.AbstractFileSystem;
 import FileSystem.Directory;
 import FileSystem.File;
 import FileSystem.FileSystem;
 import Interfaces.Command;
 import Interfaces.Repository;
-import Utils.Parameters;
+import Utils.OutputManager;
+import Utils.ParametersManager;
 
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
 public class CommandLs implements Command {
 
+    private String recursiveOutput = "";
+
     public void execute() {
         FileSystem fileSystem = FileSystem.getFileSystem();
 
-        if (Parameters.getParameters()[0].equals("")) {
-            this.execute(fileSystem.currentDirectory);
+        if (ParametersManager.noParameters()) {
+            this.execute((Repository) fileSystem.currentDirectory);
+
         } else {
-            Repository systemNode = fileSystem.getSystemNode(Parameters.getParameters()[0]);
+            Repository systemNode = fileSystem.getSystemNode(ParametersManager.getParameters());
 
             this.execute(systemNode);
+
+            if(ParametersManager.isRecursiveOption())
+                OutputManager.setOutput(recursiveOutput);
+
+            ParametersManager.flushParameters();
         }
     }
 
@@ -30,12 +38,39 @@ public class CommandLs implements Command {
     }
 
     public void execute(File file) {
+        if (ParametersManager.allDetailsOption()) {
 
+            if (ParametersManager.isRecursiveOption())
+                recursiveOutput += file.getDetails();
+            else
+                OutputManager.setOutput(file.getDetails());
+        } else {
+            if (ParametersManager.isRecursiveOption())
+                recursiveOutput += file.toString();
+            else
+                OutputManager.setOutput(file.toString());
+        }
     }
 
     public void execute(Directory directory) {
-        if (Parameters.getParameters()[0] == "") {
-            System.out.println(directory.printContent());
+
+        if (ParametersManager.isRecursiveOption()) {
+
+            if (ParametersManager.allDetailsOption())
+                recursiveOutput += directory.getDetails();
+             else
+                recursiveOutput += directory.toString();
+
+            FileSystem fileSystem = FileSystem.getFileSystem();
+
+            String content = directory.getContent();
+
+            for (String element : content.split(" ")) {
+                Repository systemNode = fileSystem.getSystemNode(element);
+                this.execute(systemNode);
+            }
         }
+        else
+            OutputManager.setOutput(directory.getContent());
     }
 }
