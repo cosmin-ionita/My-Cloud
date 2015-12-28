10f97828b22567bd78f7ab487c733943612b0ae9
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-28 21:01:44 +0200

    Added new methods on File node

--- src/FileSystem/File.java
+++ src/FileSystem/File.java
@@ -1,14 +1,11 @@
 package FileSystem;
 
 import Interfaces.Command;
-import Interfaces.Repository;
-
-import java.util.Date;
 
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
-public class File extends AbstractFileSystem {
+public class File extends AbstractFileSystemNode {
 
     private boolean isBinary;
 
@@ -18,8 +15,20 @@ public class File extends AbstractFileSystem {
         this.name = fileName;
     }
 
+    public File(String fileName, Permissions permissions) {
+        this.name = fileName;
+        this.permissions = permissions;
+    }
+
     public void accept(Command command) {
         command.execute(this);
+    }
+
+    public String getDetails() {
+        return "File: " + this.name + " " +
+                         this.dimension +
+                        ((this.isBinary) ? "binary" : "text") + " " +
+                         this.creationTime + " " + this.permissions;
     }
 
     public String toString() {
