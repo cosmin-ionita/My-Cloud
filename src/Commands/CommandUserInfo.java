b7a4fd089875f3383e003e947a52144db0813aa0
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-30 00:03:05 +0200

    Added userinfo command

---
+++ src/Commands/CommandUserInfo.java
@@ -0,0 +1,36 @@
+package Commands;
+
+import FileSystem.Directory;
+import FileSystem.File;
+import Interfaces.Command;
+import Interfaces.Repository;
+import SystemState.UserManager;
+import Utils.OutputManager;
+import Utils.ParametersManager;
+
+/**
+ * Created by Ionita Cosmin on 12/29/2015.
+ */
+public class CommandUserInfo implements Command {
+
+    public void execute() {
+        if (ParametersManager.noParameters()) {
+            OutputManager.setOutput(UserManager.getCurrentUserInfo());
+        } else {
+            OutputManager.setOutput(UserManager.getUserInfo(ParametersManager.getBruteParameters()));
+        }
+    }
+
+    public void execute(Repository repository) {
+        System.out.println("Invalid control");
+    }
+
+    public void execute(Directory directory) {
+        System.out.println("Invalid control");
+    }
+
+    public void execute(File file) {
+        System.out.println("Invalid control");
+    }
+}
+
