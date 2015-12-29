5c9549e2433f6225b77023439170a1bbb37a06e7
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-30 00:01:20 +0200

    Added getUserInfo method on UserManager + initial settings

--- src/SystemState/UserManager.java
+++ src/SystemState/UserManager.java
@@ -1,5 +1,11 @@
 package SystemState;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -7,17 +13,21 @@ import java.util.Date;
  */
 public class UserManager {
 
-    private static UserManager currentUser = new UserManager ();
+    private static UserManager currentUser = new UserManager();
 
-    private  String userName;
-    private  String name;
-    private  String surname;
+    private String userName;
+    private String name;
+    private String surname;
 
-    private  Date creationTime;
-    private  Date lastLoginTime;
+    private Date creationTime;
+    private Date lastLoginTime;
 
     private UserManager() {
-
+        userName = "guest";
+        name = "";
+        surname = "";
+        creationTime = new Date();
+        lastLoginTime = new Date();
     }
 
     public static UserManager getCurrentUser() {
@@ -28,12 +38,12 @@ public class UserManager {
         return currentUser.userName;
     }
 
-    public static void createUser(String newName,
-                                  String userSurname,
-                                  String newUserName,
+    public static void changeCurrentUser(String newName,
+                                         String userSurname,
+                                         String newUserName,
 
-                                  Date userCreationTime,
-                                  Date userLastLoginTime) {
+                                         Date userCreationTime,
+                                         Date userLastLoginTime) {
         currentUser.name = newName;
         currentUser.surname = userSurname;
         currentUser.userName = newUserName;
@@ -42,12 +52,43 @@ public class UserManager {
         currentUser.lastLoginTime = userLastLoginTime;
     }
 
-    public String getUserInfo() {
+    public static String getCurrentUserInfo() {
+        DateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-        return currentUser.userName + "\n" +
-                currentUser.name + "\n" +
-                currentUser.surname + "\n" +
-                currentUser.creationTime.toString() + "\n" +
-                currentUser.lastLoginTime.toString() + "\n";
+        return  "Username: " + currentUser.userName + "\n" +
+                "Name: " + currentUser.name + "\n" +
+                "Surname: " + currentUser.surname + "\n" +
+                "Created: " + formatter.format(currentUser.creationTime) + "\n" +
+                "Last Login: " + formatter.format(currentUser.lastLoginTime) + "\n";
+    }
+
+    public static String getUserInfo(String userName) {
+
+        String userInfo = "";
+
+        try {
+            RandomAccessFile stream = new RandomAccessFile("Users.txt", "rw");
+
+            String line = stream.readLine();
+
+            while (line != null) {
+
+                if(userName.equals(line.split(" ")[0])) {
+                    userInfo =  "Username: " + userName + "\n" +
+                                "Name: " + line.split(" ")[2] + "\n" +
+                                "Surname: " + line.split(" ")[3] + "\n" +
+                                "Created: " + line.split(" ")[4].replace("$", " ") + "\n" +
+                                "Last Login: " + line.split(" ")[5].replace("$", " ") + "\n";
+                }
+
+                line = stream.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        return userInfo;
     }
 }
