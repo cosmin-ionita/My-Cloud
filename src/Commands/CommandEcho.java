052e48722b4ac560dd5fa49bdd0d1baaabebb608
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-30 00:04:16 +0200

    Added echo command

---
+++ src/Commands/CommandEcho.java
@@ -0,0 +1,30 @@
+package Commands;
+
+import FileSystem.Directory;
+import FileSystem.File;
+import Interfaces.Command;
+import Interfaces.Repository;
+import Utils.OutputManager;
+import Utils.ParametersManager;
+
+/**
+ * Created by Ionita Cosmin on 12/29/2015.
+ */
+public class CommandEcho implements Command {
+
+    public void execute() {
+        OutputManager.setOutput(ParametersManager.getBruteParameters());
+    }
+
+    public void execute(Directory directory) {
+        System.out.println("Invalid control");
+    }
+
+    public void execute(Repository repository) {
+        System.out.println("Invalid control");
+    }
+
+    public void execute(File file) {
+        System.out.println("Invalid control");
+    }
+}
