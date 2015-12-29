1dbc40863d9d2d4ffa1d4e3b5c63e33d82c35c49
Ionita Cosmin <cosmin.ionita@equilobe.com>
2015-12-29 23:58:36 +0200

    Added date functionality on File node

--- src/FileSystem/File.java
+++ src/FileSystem/File.java
@@ -2,6 +2,10 @@ package FileSystem;
 
 import Interfaces.Command;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
@@ -11,13 +15,10 @@ public class File extends AbstractFileSystemNode {
 
     private byte[] content;
 
-    public File(String fileName) {
-        this.name = fileName;
-    }
-
     public File(String fileName, Permissions permissions) {
         this.name = fileName;
         this.permissions = permissions;
+        this.creationTime = new Date();
     }
 
     public void accept(Command command) {
@@ -25,10 +26,13 @@ public class File extends AbstractFileSystemNode {
     }
 
     public String getDetails() {
+
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
         return "File: " + this.name + " " +
-                         this.dimension +
-                        ((this.isBinary) ? "binary" : "text") + " " +
-                         this.creationTime + " " + this.permissions;
+                         this.dimension + " " +
+                         ((this.isBinary) ? "binary" : "text") + " " +
+                         format.format(this.creationTime) + " " + this.permissions + "\n";
     }
 
     public String toString() {
