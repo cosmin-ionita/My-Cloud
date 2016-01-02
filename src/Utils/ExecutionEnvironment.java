7a7f7ab182fe66ce7d5eb37454df2cdeba0b453e
Ionita Cosmin <cosmin.ionita@equilobe.com>
2016-01-02 12:44:27 +0200

    Refactored

--- src/Utils/ExecutionEnvironment.java
+++ src/Utils/ExecutionEnvironment.java
@@ -1,7 +1,9 @@
 package Utils;
 
+import Cloud.CloudService;
 import FileSystem.FileSystem;
 import Interfaces.Command;
+import SystemState.Logger;
 import SystemState.UserManager;
 
 /**
@@ -12,41 +14,93 @@ public class ExecutionEnvironment {
     private static String lastCommand = "";
 
     public static void invoke(String bruteCommand) {
-        Command command = CommandFactory.getCommand(bruteCommand.split(" ")[0]);
 
-        lastCommand = bruteCommand.split(" ")[0];
+        if (bruteCommand.split(" ")[0].equals("upload")) {
 
-        // Remove command from bruteCommand
-        String parameters = bruteCommand.replace(bruteCommand.split(" ")[0], "");
+            ParametersManager.setParameters(bruteCommand.split(" ")[1]);
+            CloudService.executeUpload();
 
-        // Remove the space after the command name
-        if(parameters.length() > 1)
-           parameters = parameters.substring(1);
+        } else if (bruteCommand.split(" ")[0].equals("sync")) {
 
-        ParametersManager.setParameters(parameters);
+            ParametersManager.setParameters(bruteCommand.split(" ")[1]);
+            CloudService.executeSync();
 
-        // For invalid commands, does nothing
-        if(command != null) {
-            command.execute();
+        } else {
+            Command command = CommandFactory.getCommand(bruteCommand.split(" ")[0]);
+
+            lastCommand = bruteCommand.split(" ")[0];
+
+            // Remove command from bruteCommand
+            String parameters = bruteCommand.replace(bruteCommand.split(" ")[0], "");
+
+            // Remove the space after the command name
+            if (parameters.length() > 1)
+                parameters = parameters.substring(1);
+
+            ParametersManager.flushParameters();
+            ParametersManager.setParameters(parameters);
+
+            // For invalid commands, does nothing
+            if (command != null) {
+                command.execute();
+            } else {
+                OutputManager.setOutput("Invalid command!");
+            }
         }
     }
 
     public static boolean wasCdCommand() {
-        if(lastCommand.equals("cd")) {
+        if (lastCommand.equals("cd")) {
             return true;
         }
         return false;
     }
 
     public static boolean wasLoginCommand() {
-        if(lastCommand.equals("login")) {
+        if (lastCommand.equals("login")) {
             return true;
         }
         return false;
     }
 
+    public static boolean wasLogoutComand() {
+        if (lastCommand.equals("logout")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean wasEchoComand() {
+        if (lastCommand.equals("echo")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean wasUserInfoComand() {
+        if (lastCommand.equals("userinfo")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean wasLsComand() {
+        if (lastCommand.equals("ls")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPooOutput() {
+        return OutputManager.isPooOutput();
+    }
+
     public static String getCurrentFileSystemPath() {
         return FileSystem.getFileSystem().currentDirectory.getCurrentPath();
+    }
+
+    public static void storeLogs() {
+        Logger.storeLogs();
     }
 
     public static String getCurrentUser() {
