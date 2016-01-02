e8fe1a32d2f4b027df889cc2d25c93ebf0f4da08
Ionita Cosmin <cosmin.ionita@equilobe.com>
2016-01-02 12:43:44 +0200

    Added helper methods

--- src/FileSystem/FileSystem.java
+++ src/FileSystem/FileSystem.java
@@ -3,6 +3,8 @@ package FileSystem;
 import Interfaces.Repository;
 import SystemState.UserManager;
 
+import java.util.Date;
+
 /**
  * Created by Ionita Cosmin on 12/22/2015.
  */
@@ -15,6 +17,11 @@ public class FileSystem {
     private FileSystem() {
         currentDirectory.setCurrentPath("/home");
         currentDirectory.permissions = new Permissions(true, true, UserManager.getCurrentUserName());
+        currentDirectory.creationTime = new Date();
+    }
+
+    public void restoreFileSystem() {
+        fileSystem = new FileSystem();
     }
 
     public static FileSystem getFileSystem() {
@@ -23,5 +30,9 @@ public class FileSystem {
 
     public Repository getSystemNode(String nodeName) {
         return currentDirectory.getNode(nodeName);
+    }
+
+    public Repository getSystemNode(Directory directory, String nodeName) {
+        return directory.getNode(nodeName);
     }
 }
