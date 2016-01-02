afc4275783eb381beb9782d66cb5a1461df9fd22
Ionita Cosmin <cosmin.ionita@equilobe.com>
2016-01-02 12:44:54 +0200

    Added permission handling

--- src/FileSystem/Directory.java
+++ src/FileSystem/Directory.java
@@ -1,7 +1,10 @@
 package FileSystem;
 
+import Cloud.Storage.MachineId;
+import Exceptions.MyInvalidPathException;
 import Interfaces.Command;
 import Interfaces.Repository;
+import SystemState.Logger;
 import SystemState.UserManager;
 
 import java.text.DateFormat;
@@ -12,7 +15,7 @@ import java.util.Date;
 /**
  * Created by Ionita Cosmin on 12/21/2015.
  */
-public class Directory extends AbstractFileSystemNode {
+public class Directory extends FileSystemNode {
 
     private ArrayList<Repository> data;
 
@@ -23,10 +26,26 @@ public class Directory extends AbstractFileSystemNode {
     public Directory(String name) {
         this.data = new ArrayList<>();
         this.name = name;
+        this.dimension = 0;
+    }
+
+    public Directory(Directory directory) {
+        data = new ArrayList<>(directory.data);
+
+        name = directory.name;
+        dimension = directory.dimension;
+        currentPath = directory.currentPath;
+        parent = directory.parent;
+        creationTime = directory.creationTime;
+        permissions = directory.permissions;
     }
 
     public void accept(Command command) {
-        command.execute(this);
+        try {
+            command.execute(this);
+        } catch (MyInvalidPathException e) {
+            Logger.log(e.toString());
+        }
     }
 
     public void deleteNode(String nodeName) {
@@ -43,7 +62,37 @@ public class Directory extends AbstractFileSystemNode {
     }
 
     public boolean isEmpty() {
-        if(data.size() == 0)
+        if (data.size() == 0)
+            return true;
+        return false;
+    }
+
+    public boolean canRead() {
+        String userName = UserManager.getCurrentUserName();
+
+        if((this.permissions.read &&
+                (userName.equals(this.permissions.userName) || this.name == "home") &&
+                !userName.equals("guest")) ||
+                userName.equals("root"))
+
+            return true;
+        return false;
+    }
+
+    public boolean canWrite() {
+        String userName = UserManager.getCurrentUserName();
+
+        if((this.permissions.write &&
+                (userName.equals(this.permissions.userName) || this.name == "home") &&
+                !userName.equals("guest")) ||
+                userName.equals("root"))
+
+            return true;
+        return false;
+    }
+
+    public boolean canMove() {
+        if(!UserManager.getCurrentUserName().equals("guest"))
             return true;
         return false;
     }
@@ -65,14 +114,30 @@ public class Directory extends AbstractFileSystemNode {
         newDirectory.parent = this;
         newDirectory.creationTime = new Date();
         newDirectory.currentPath = this.currentPath + "/" + directoryName;
-        newDirectory.permissions = new Permissions( permissions.contains("r"),
-                                                    permissions.contains("w"),
-                                                    UserManager.getCurrentUserName());
+        newDirectory.permissions = new Permissions(permissions.contains("r"),
+                permissions.contains("w"),
+                UserManager.getCurrentUserName());
+
         data.add(newDirectory);
     }
 
-    public void addFile(String fileName) {
-        data.add(new File(fileName, FileSystem.getFileSystem().currentDirectory.permissions));
+    public void addNode(Repository repository) {
+        data.add(repository);
+    }
+
+    public void addFile(String fileName, int dimension) {
+        data.add(new File(fileName, FileSystem.getFileSystem().currentDirectory.permissions, dimension));
+
+        if (dimension > 0) {
+
+            Directory directory = this;
+
+            while (directory != null) {
+                directory.dimension += dimension;
+
+                directory = directory.getParent();
+            }
+        }
     }
 
     public Repository getNode(String nodeName) {
@@ -90,10 +155,19 @@ public class Directory extends AbstractFileSystemNode {
 
                 if (file.name.equals(nodeName))
                     return file;
+            } else if (repository.getClass().toString().split(" ")[1].equals("Cloud.Storage.MachineId")) {
+                MachineId id = (MachineId) repository;
+
+                if (id.toString().equals(nodeName))
+                    return id;
             }
         }
 
         return null;
+    }
+
+    public boolean contains(Repository node) {
+        return this.data.contains(node);
     }
 
     public String getContent() {
@@ -109,7 +183,7 @@ public class Directory extends AbstractFileSystemNode {
 
         DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-        return  "Directory: " + this.name + " " +
+        return "Directory: " + this.name + " " +
                 this.dimension + " " +
                 format.format(this.creationTime) + " " + this.permissions + "\n";
     }
@@ -118,7 +192,11 @@ public class Directory extends AbstractFileSystemNode {
         return this.parent;
     }
 
+    public int getDimension() {
+        return this.dimension;
+    }
+
     public String toString() {
-       return this.name;
+        return this.name;
     }
 }
